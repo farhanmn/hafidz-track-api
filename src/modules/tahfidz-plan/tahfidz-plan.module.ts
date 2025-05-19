eee1c9951187eb56b2a0c5b62b714b196f8f33e1
farhanmn <najibfarhan3@gmail.com>
2025-05-20 05:58:14 +0700

    provider tahfidz plan

--- src/modules/tahfidz-plan/tahfidz-plan.module.ts
+++ src/modules/tahfidz-plan/tahfidz-plan.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TahfidzPlanService } from './tahfidz-plan.service';
 import { TahfidzPlanController } from './tahfidz-plan.controller';
+import { StudentsService } from '../students/students.service';
+import { UsersService } from '../users/users.service';
 
 @Module({
   controllers: [TahfidzPlanController],
-  providers: [TahfidzPlanService],
+  providers: [TahfidzPlanService, StudentsService, UsersService]
 })
 export class TahfidzPlanModule {}
