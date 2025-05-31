06de9f1eee530ded0352499dac3de80836351042
farhanmn <najibfarhan3@gmail.com>
2025-05-31 23:02:05 +0700

    get expires jwt from env

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -10,8 +10,8 @@ import { JwtStrategy } from './strategies/jwt.strategies';
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1d' }
+      secret: process.env.JWT_SECRET || 'secret',
+      signOptions: { expiresIn: process.env.JWT_EXPIRES || '1d' }
     })
   ],
   providers: [UsersService, AuthService, JwtStrategy],
